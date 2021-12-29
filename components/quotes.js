98d56fbef1ffbe8016ff94c555e4bc14f8295012
Chan Alston <51868695+AlstonChan@users.noreply.github.com>
2021-12-29 21:53:19 +0800

    change data fetching type to SSG
    remove SWR and update eslint package, react-dom update to beta unsuccessful, changed quote.js loading

--- components/quotes.js
+++ components/quotes.js
@@ -1,22 +1,16 @@
-import useSWR from "swr";
 import Image from "next/image";
-import axios from "axios";
 
 import styles from "../styles/quotes.module.css";
 import { useState } from "react";
 
-const fetch = (url) => axios.get(url);
-
-export default function Quotes() {
-  const { data, err } = useSWR("https://type.fit/api/quotes", fetch);
+export default function Quotes({ data }) {
   const [count, setCount] = useState(0);
 
   function randomQuote() {
     setCount(Math.floor(Math.random() * 1643));
   }
 
-  if (err) return <div> failed to load </div>;
-  if (!data) return <div> loading... </div>;
+  // if (!data) return <div> loading... </div>;
 
   return (
     <section className={styles.quotes}>
@@ -24,35 +18,40 @@ export default function Quotes() {
         <Image
           className={styles.codeImage}
           src="/CarbonQuotesBig.png"
+          alt="Sample code of the random quote api"
           width="828px"
           height="572px"
-          alt="Sample code of the random quote api"
+          priority
         />
       </div>
       <div className={styles.quoteMainContainer}>
         <div className={styles.quoteContainerTitle}>
           <h1> Random Quotes </h1>
         </div>
-        <div className={styles.quoteContainerContent}>
-          {data.data[count].text}
-          <br />
-          <br />
-          <address>
-            by{" "}
-            <a rel="author">
-              <strong>
-                <i>
-                  {data.data[count].author
-                    ? data.data[count].author
-                    : "author unknown"}
-                </i>
-              </strong>{" "}
-            </a>
-          </address>
-          <button className={styles.randomBtn} onClick={randomQuote}>
-            Random{" "}
-          </button>
-        </div>
+        {!data ? (
+          <div className={styles.quoteContainerContent}>
+            <p className={styles.loadingContent}>Loading...</p>
+          </div>
+        ) : (
+          <div className={styles.quoteContainerContent}>
+            {data[count].text}
+            <br />
+            <br />
+            <address>
+              by{" "}
+              <a rel="author">
+                <strong>
+                  <i>
+                    {data[count].author ? data[count].author : "author unknown"}
+                  </i>
+                </strong>{" "}
+              </a>
+            </address>
+            <button className={styles.randomBtn} onClick={randomQuote}>
+              Random{" "}
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import axios from "axios";
 import Quotes from "../components/quotes";
 import Aboutme from "../components/aboutme";
 import Footer from "../components/footer";
 
-export default function Home() {
+const url = "https://type.fit/api/quotes";
+
+export default function Home({ data }) {
   return (
     <>
       <Head>
@@ -39,10 +42,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="backContainer">
-        <Quotes />
+        <Quotes data={data} />
         <Aboutme />
       </div>
       <Footer />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await axios.get(url);
+  const data = res.data;
+
+  return {
+    props: { data },
+  };
 }
